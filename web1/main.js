db6a04c149a1280fe00bea286ec88def8ff0385b
vinita2000 <vinitayadavlkw225@gmail.com>
2020-05-01 12:32:36 +0530

    complete source code
    complete source code with some extra feature and images of different products and categories.

---
+++ web1/main.js
@@ -0,0 +1,10 @@
+$(".slider-one")
+.not(".slick-initialized")
+.slick({
+
+	prevArrow:".site-slider.prev",
+	nextArrow: ".site-slider.next",
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	autoplaySpeed: 3000
+});
